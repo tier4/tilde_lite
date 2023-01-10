249946d23e59de9e747fcdeb4eac3229d96220e9
xygyo77 <akm@isp.co.jp>
2023-01-10 14:08:18 +0900

    add mtt_checker (#6)
    update tilde_timing_monitor

---
+++ src/mtt_checker/setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'mtt_checker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('~/launch/*_launch.py')),
+        ('share/' + package_name, glob(os.path.join('launch', '*_launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='akm',
+    maintainer_email='akm@isp.co.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mtt_checker = mtt_checker.mtt_checker:main',
+        ],
+    },
+)
